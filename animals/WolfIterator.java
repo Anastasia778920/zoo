956d5b8b9c6796585003ebb641955a1154e70e1d
Настя <zakharovanas0905@gmail.com>
2023-03-05 19:48:45 +0300

    WolfIterator

---
+++ animals/WolfIterator.java
@@ -0,0 +1,23 @@
+package animals;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+
+public class WolfIterator implements Iterator {
+    private ArrayList<Wolf> wolves;
+    private int cursor;
+
+    public WolfIterator(ArrayList<Wolf> wolves){
+        this.wolves = wolves;
+    }
+
+    @Override
+    public boolean hasNext() {
+        return cursor < wolves.size();
+    }
+
+    @Override
+    public Wolf next() {
+        return wolves.get(cursor++);
+    }
+}

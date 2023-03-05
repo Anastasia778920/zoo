699769746764f1ff7f287322b303ddbbb99e3887
Настя <zakharovanas0905@gmail.com>
2023-03-05 19:43:07 +0300

    AnimalComporator

---
+++ animals/AnimalAgeComparator.java
@@ -0,0 +1,16 @@
+package animals;
+
+import java.util.Comparator;
+
+public class AnimalAgeComparator implements Comparator<Animal> {
+    @Override
+    public int compare(Animal o1, Animal o2) {
+        if(o1.getBirthYear() < o2.getBirthYear()){
+            return 1;
+        }
+        else if(o1.getBirthYear() > o2.getBirthYear()){
+            return -1;
+        }
+        return 0;
+    }
+}

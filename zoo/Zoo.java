5e636f42754e986f28aa0e611d5c613b88ce134a
Настя <zakharovanas0905@gmail.com>
2023-03-05 19:52:31 +0300

    Zoo

---
+++ zoo/Zoo.java
@@ -0,0 +1,43 @@
+package zoo;
+
+import animals.Lion;
+import animals.Snake;
+import animals.Wolf;
+import Cage.AnimalCage;
+
+public class Zoo {
+    private AnimalCage<Wolf> wolfCage;
+    private AnimalCage<Lion> lionCage;
+    private AnimalCage<Snake> snakeCage;
+
+    public Zoo(AnimalCage<Wolf> wolfCage, AnimalCage<Lion> lionCage, AnimalCage<Snake> snakeTerrarium) {
+        this.wolfCage = wolfCage;
+        this.lionCage = lionCage;
+        this.snakeCage = snakeTerrarium;
+    }
+
+
+    public AnimalCage<Wolf> getWolfCage() {
+        return wolfCage;
+    }
+
+    public void setWolfCage(AnimalCage<Wolf> wolfCage) {
+        this.wolfCage = wolfCage;
+    }
+
+    public AnimalCage<Lion> getLionCage() {
+        return lionCage;
+    }
+
+    public void setLionCage(AnimalCage<Lion> lionCage) {
+        this.lionCage = lionCage;
+    }
+
+    public AnimalCage<Snake> getSnakeTerrarium() {
+        return snakeCage;
+    }
+
+    public void setSnakeTerrarium(AnimalCage<Snake> snakeTerrarium) {
+        this.snakeCage = snakeTerrarium;
+    }
+}

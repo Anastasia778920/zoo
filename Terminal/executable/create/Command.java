b64e95288a0e9354843f804f2d6e47a20a8822b9
Настя <zakharovanas0905@gmail.com>
2023-03-08 21:57:27 +0300

    HW7

---
+++ Terminal/executable/create/Command.java
@@ -0,0 +1,22 @@
+package Terminal.executable.create;
+
+import java.util.ArrayList;
+
+public class Command {
+
+    public Command(String animal, String action, ArrayList<String> param) {
+    }
+
+    public ArrayList<String> getParameters() {
+        return null;
+    }
+
+	public Command getCommandEnum() {
+		return null;
+	}
+
+    public CommandExecutable getMethod() {
+        return null;
+    }
+
+}
